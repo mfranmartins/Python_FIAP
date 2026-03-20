0e5b122b67ef7bd93323b2092eaae22e4ebe78a4
mfranmartins <mfran.martiins@gmail.com>
2026-03-20 20:50:25 -0300

    Aula 2, variáveis e operadores

---
+++ aula02-variáveis-operadores/aula02-variáveis-operadores.py
@@ -0,0 +1,32 @@
+print("Hello world")
+
+print(7+4)
+print("7+4")
+print("7"+"4") #concatenação de strings
+
+#Comentários em python de 1 linha
+'''
+Comentários de
+múltiplas
+linhas
+'''
+
+# VARIÁVEIS
+nome = "Roberto" # string
+idade = 26 # int
+peso = 70.2 # float
+
+print(nome, idade, peso)
+print(f"Oiii, {nome}!!!")
+
+# INPUT -- SIMULA UM FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
+
+name = input("Digite o seu nome: ")
+print(f"Olá {name}, seja bem vindo")
---
+++ aula02-variáveis-operadores/desafios.py
@@ -0,0 +1,12 @@
+name = input("Digite o seu nome: ")
+print(f"Olá {name}, seja bem vindo")
+
+dia = input("Digite o seu dia de nascimento: ")
+mes = input("Digite o seu mês de nascimento: ")
+ano = input("Digite o seu ano de nascimento: ")
+print(f"{dia}/{mes}/{ano}")
+
+val1 = int(input("Digite o primeiro valor para ser somado: "))
+val2 = int(input("Digite o segundo valor para ser somado: "))
+resultado = val1 + val2
+print(f"O resultado da soma entre {val1} e {val2} é: {resultado} ")
